a6a428d0e60c6af41ae4f0a4d8f5aa5823587449
Conor Woods <conorw@gmail.com>
2024-07-03 13:49:18 +0100

    Init tests for postcode search

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,28 @@
 import { describe, it, expect } from 'vitest';
-
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
+import { searchPostcode } from './lib/postcodes';
+describe('Postcode tests', () => {
+	it('A empty postcode should throw an exception', () => {
+		expect(() => searchPostcode('')).rejects.toThrow('Postcode is required');
+	});
+	it('A invalid 123 postcode should throw an exception', () => {
+		expect(() => searchPostcode('123')).rejects.toThrow('Invalid postcode');
+	});
+	it('A invalid bt%^ postcode should throw an exception', () => {
+		expect(() => searchPostcode('bt%^')).rejects.toThrow('Invalid postcode');
+	});
+	it('A valid postcode BT48 7AL should return a result', async () => {
+		const result = await searchPostcode('BT48 7AL');
+		console.log(result);
+		expect(result.postcode).toBe('BT48 7AL');
+	});
+	it('A valid postcode BT487AL should return a result', async () => {
+		const result = await searchPostcode('BT487AL');
+		console.log(result);
+		expect(result.postcode).toBe('BT48 7AL');
+	});
+	it('A valid postcode bt487al should return a result', async () => {
+		const result = await searchPostcode('bt487al');
+		console.log(result);
+		expect(result.postcode).toBe('BT48 7AL');
 	});
 });
---
+++ src/lib/postcodes.ts
@@ -0,0 +1,19 @@
+// add method to use the postcode.io API to get the address from a postcode
+// https://postcodes.io/docs
+// interesting fields: postcode, country, region, admin_district, parish
+export const searchPostcode = async (postcode: string): Promise<any> => {
+  // cater for the case where the postcode is not valid, throw an error
+  if (!postcode) {
+    throw new Error("Postcode is required");
+  }
+  // allow upper and lower case letters, numbers and spaces e.g. bt546db is allowed
+  if (!postcode.match(/^[A-Za-z]{1,2}[0-9][0-9A-Za-z]?\s?[0-9][A-Za-z]{2}$/)) {
+    throw new Error("Invalid postcode");
+  }
+
+  const response = await fetch(
+    `https://api.postcodes.io/postcodes/${postcode}`
+  );
+  const data = await response.json();
+  return data.result;
+};
