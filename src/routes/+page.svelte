363e99dd9b8e70928c4240482a01f01b629804be
Conor Woods <conorw@gmail.com>
2024-07-03 14:14:19 +0100

    Simple search screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { searchPostcode } from "$lib/postcodes";
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
+
+  let postcodedata: any;
+  let searchText: string = "";
+  let errorText = "";
+
+  onMount(async () => {
+    const postcode = $page.url.searchParams.get("postcode");
+    if (postcode) {
+      searchText = postcode.toUpperCase();
+      postcodedata = await search(postcode);
+    }
+  });
+
+  async function search(postcode: string) {
+    try {
+      errorText = "";
+      postcodedata = null;
+      const data = await searchPostcode(postcode);
+      postcodedata = data;
+    } catch (error: any) {
+      errorText = error.message;
+    }
+  }
+</script>
+
+<div>
+  <h1>{postcodedata?.postcode || "Search for a postcode"}</h1>
+  <input
+    type="text"
+    id="search"
+    bind:value={searchText}
+    placeholder="Search for postcode"
+  />
+  <button on:click={() => search(searchText)}>Search</button>
+  {#if errorText}
+    <p>{errorText}</p>
+  {/if}
+</div>
