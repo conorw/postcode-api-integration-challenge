73bd420a3d3f1f2a9421f43d8b473e259f1bb9f2
Conor Woods <conorw@gmail.com>
2024-07-04 10:33:31 +0100

    set cache headers

--- src/routes/[postcode]/+page.server.ts
+++ src/routes/[postcode]/+page.server.ts
@@ -4,8 +4,9 @@ import type { PageServerLoad } from "./$types";
 // SSR is enabled for this page. optional, defaults to true
 export const ssr = true;
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, setHeaders }) => {
   const postcode = params.postcode;
   const postcodedata = await searchPostcode(postcode);
+  setHeaders({ "cache-control": "max-age=3600" });
   return { postcodedata };
 };
